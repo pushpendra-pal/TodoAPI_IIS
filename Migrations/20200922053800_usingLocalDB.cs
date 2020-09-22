6867a67a2f617c99e28df167282911c4f4f65c95
pushpendra_pal <70932823+pushpendra-pal@users.noreply.github.com>
2020-09-22 11:32:36 +0530

    Add project files.

---
+++ Migrations/20200922053800_usingLocalDB.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TodoAPI.Migrations
+{
+    public partial class usingLocalDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
